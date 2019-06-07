9b6bdd0a889e277d0532ecbb00cf46607a856b17
sagar1947 <sagarpandey1000@gmail.com>
2019-06-07 18:33:58 +0530

    Create app.js
    This is the script part related to our web page where the logic to implement the file explorer resides.

---
+++ app.js
@@ -0,0 +1,14 @@
+var myApp = angular.module("myApp", ['ngRoute']);
+
+myApp.controller("FileExplorerCntrl", ['$scope', function($scope) {
+    $scope.delete = function(data) {
+        data.nodes = [];
+    };
+    $scope.add = function(data) {
+        var post = data.nodes.length + 1;
+        var newName = data.name + '-' + post;
+        data.nodes.push({name: newName,nodes: []});
+    };
+    $scope.tree = [{name: "Folder", nodes: []}];
+}]);
+
